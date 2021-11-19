4e9f92ba3b321f7f0ef679d27918d8943709b92c
vuhaimc4210 <81548566+vuhaimc4210@users.noreply.github.com>
2021-11-19 17:46:37 +0700

    update and add item

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -20,15 +20,14 @@ public class Bomber extends Entity {
     private int life;
     private boolean reborn;
 
-    public static void resetBombLimit() {
-        bombLimit = 1;
+    public Bomber(int x, int y, Image img) {
+        super(x, y, img);
+        upSpeed = false;
+        life = 3;
+        reborn = false;
     }
 
-    public static void resetSpeed() {
-        speed = 1;
-    }
-
-
+    //Tại hoạt ảnh của bomber
     public static void load() {
         ArrayList<Image> up = new ArrayList<>();
         ArrayList<Image> right = new ArrayList<>();
@@ -36,81 +35,82 @@ public class Bomber extends Entity {
         ArrayList<Image> left = new ArrayList<>();
         ArrayList <Image> dead = new ArrayList<>();
 
-        // 0: up
+        // 0: lên trên
         up.add(Sprite.player_up.getFxImage());
         up.add(Sprite.player_up_1.getFxImage());
         up.add(Sprite.player_up_2.getFxImage());
         constImage.add(up);
 
-        // 1: right
+        // 1: sang phải
         right.add(Sprite.player_right.getFxImage());
         right.add(Sprite.player_right_1.getFxImage());
         right.add(Sprite.player_right_2.getFxImage());
         constImage.add(right);
 
-        // 2: down
+        // 2: xuống dưới
         down.add(Sprite.player_down.getFxImage());
         down.add(Sprite.player_down_1.getFxImage());
         down.add(Sprite.player_down_2.getFxImage());
         constImage.add(down);
 
-        // 3: left
+        // 3: sang trái
         left.add(Sprite.player_left.getFxImage());
         left.add(Sprite.player_left_1.getFxImage());
         left.add(Sprite.player_left_2.getFxImage());
         constImage.add(left);
 
-        //dead
+        //chết
         dead.add(Sprite.player_dead1.getFxImage());
         dead.add(Sprite.player_dead2.getFxImage());
         dead.add(Sprite.player_dead3.getFxImage());
         constImage.add(dead);
     }
-    public Bomber(int x, int y, Image img) {
-        super( x, y, img);
-        upSpeed = false;
-        life = 3;
-        reborn = false;
+
+    int dirX(int dir) { //Di chuyển trái phải
+        switch (dir) {
+            case 1:
+                return 1;
+            case 3:
+                return -1;
+            default:
+                return 0;
+        }
     }
 
-    int dirX(int dir) {
-        if (dir == 1) {
-            return 1;
+    int dirY(int dir) { //Di chuyển lên xuống
+        switch (dir) {
+            case 0:
+                return -1;
+            case 2:
+                return 1;
+            default:
+                return 0;
         }
-        if (dir == 3) {
-            return -1;
-        }
-        return 0;
-    }
-
-    int dirY(int dir) {
-        if (dir == 0) {
-            return -1;
-        }
-        if (dir == 2) {
-            return 1;
-        }
-        return 0;
     }
 
     int moveX(int curDir, Entity entity) {
         switch (curDir) {
-            case 1:
-            case 3:
+            //Nếu đứng giữa ô thì không di chuyển sang 2 bên
+            case 1: //Đi sang phải
+            case 3: //Đi sang trái
                 return 0;
-            case 0:
+            case 0: //Đi lên trên
                 if (x < entity.getX() && entity.getX() - x > 10) {
+                    //Nếu vị trí đứng nhỏ hơn vị trí của entity gặp phải thì đi sang trái
                     return -1;
                 }
                 if (x > entity.getX() && x - entity.getX() > 20) {
+                    //Nếu vị trí đứng lớn hơn vị trí của entity gặp phải thì đi sang phải
                     return 1;
                 }
                 break;
-            case 2:
+            case 2: //Đi xuống dưới
                 if (x < entity.getX() && entity.getX() - x > 10) {
+                    //Nếu vị trí đứng nhỏ hơn vị trí của entity gặp phải thì đi sang trái
                     return -1;
                 }
                 if (x > entity.getX() && x - entity.getX() > 20) {
+                    //Nếu vị trí đứng lớn hơn vị trí của entity gặp phải thì đi sang phải
                     return 1;
                 }
                 break;
@@ -118,20 +118,23 @@ public class Bomber extends Entity {
         return 0;
     }
 
-    int moveY(int curDir, Entity entity) {
+    int moveY(int curDir, Entity entity) { //Di chuyển trái, phải
         switch (curDir) {
-            case 0:
-            case 2:
+            //Nếu đứng giữa ô thì không di chuyển sang 2 bên
+            case 0: //Đi lên trên
+            case 2: //Đi xuống dưới
                 return 0;
-            case (1):
+            case (1): //Đi sang phải
                 if (y > entity.getY() && y - entity.getY() > 20) {
+                    //Nếu vị trí đứng lớn hơn vị trí của entity gặp phải thì đi xuống
                     return 1;
                 }
                 if (y < entity.getY() && entity.getY() - y > 20) {
+                    //Nếu vị trí đứng nhỏ hơn vị trí của entity gặp phải thì đi lên
                     return -1;
                 }
                 break;
-            case (3):
+            case (3): //Đi sang trái
                 if (y > entity.getY() && y - entity.getY() > 20) {
                     return 1;
                 }
@@ -143,13 +146,16 @@ public class Bomber extends Entity {
         return 0;
     }
 
+    //Kiểm tra di chuyển
     public int canMove(int curDir) {
         int aX = 0;
         int aY = 0;
         int newX = x + dirX(curDir) * speed;
         int newY = y + dirY(curDir) * speed;
         boolean meetBlock = false;
+        //Tạo khối tại vị trí di chuyển tới
         Rectangle2D rect = new Rectangle2D(newX, newY, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
+        //Tạo khối tại vị trí vừa đi qua
         Rectangle2D initRect = new Rectangle2D(x, y, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
 //      bomb
 //        for (Bomb bomb : BombermanGame.bombs) {
@@ -188,7 +194,6 @@ public class Bomber extends Entity {
             aX += moveX(curDir, brick);
             aY += moveY(curDir, brick);
         }
-
         if (!meetBlock) { /// take bonus later
             x = newX;
             y = newY;
@@ -214,18 +219,56 @@ public class Bomber extends Entity {
     }
 
     void dropBomb(long l) {
-//        if (BombermanGame.bombs.size() == bombLimit) return;
-//        int xb = (x / 32);
-//        int yb = (y / 32);
-//        Rectangle2D rect = new Rectangle2D(xb * Sprite.SCALED_SIZE, yb * Sprite.SCALED_SIZE, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
-//        for (Bomb b : BombermanGame.bombs) {
-//            if (rect.intersects(b.getX(), b.getY(), Sprite.SCALED_SIZE, Sprite.SCALED_SIZE)) {
-//                return;
-//            }
-//        }
-//        Bomb newBomb = new Bomb(xb, yb, Sprite.bomb.getFxImage(), l);
-//        BombermanGame.bombs.add(newBomb);
+        if (BombermanGame.bombs.size() == bombLimit) {
+            //Nếu bom đạt giới hạn thì không thể đặt
+            return;
+        }
+        //Xử lí sai số do không đứng giữa ô
+        int xb = (x / 32);
+        int yb = (y / 32);
+        Rectangle2D rect = new Rectangle2D(xb * Sprite.SCALED_SIZE, yb * Sprite.SCALED_SIZE, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
+        for (Bomb b : BombermanGame.bombs) {
+            //Kiểm tra nếu tại vị trí có bom thì không thể đặt thêm bom
+            if (rect.intersects(b.getX(), b.getY(), Sprite.SCALED_SIZE, Sprite.SCALED_SIZE)) {
+                return;
+            }
+        }
+        //Tạo bom tại vị trí xb, yb với thời gian nổ là l
+        Bomb newBomb = new Bomb(xb, yb, Sprite.bomb.getFxImage(), l);
+        BombermanGame.bombs.add(newBomb);
+        //Âm thanh bom
 //        BombermanGame.playSound(BombermanGame.clipBombSet);
+    }
+
+    public void getBonus() {
+        Rectangle2D rect = new Rectangle2D(this.x, this.y, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
+        for (Item i : BombermanGame.items) {
+            if (!i.isOpen() || i.isDeath()) continue;
+            if (rect.intersects(i.getX(), i.getY(), Sprite.SCALED_SIZE, Sprite.SCALED_SIZE)) {
+                if (i.getType() == 4) {
+                    if (BombermanGame.enemies.size() == 0) {
+                        BombermanGame.winGame = true;
+//                        BombermanGame.playSound(BombermanGame.clipExitOpen);
+                    }
+                    return;
+                }
+
+                i.setDeath(true);
+//                BombermanGame.playSound(BombermanGame.clipitemGet);
+                switch (i.getType()) {
+                    case (3):
+                        this.setUpSpeed(true);
+                        break;
+                    case (2):
+                        Bomb.upBombLen();
+                        break;
+                    case (1):
+                        this.upLimitBomb();
+                        break;
+                }
+                return;
+            }
+        }
     }
 
     public void setUpSpeed(boolean upSpeed) {
@@ -244,35 +287,12 @@ public class Bomber extends Entity {
         ++bombLimit;
     }
 
-    public void getBonus() {
-        Rectangle2D rect = new Rectangle2D(this.x, this.y, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
-//        for (Item i : BombermanGame.items) {
-//            if (!i.isOpen() || i.isDeath()) continue;
-//            if (rect.intersects(i.getX(), i.getY(), Sprite.SCALED_SIZE, Sprite.SCALED_SIZE)) {
-//                if (i.getType() == 4) {
-//                    if (BombermanGame.enemies.size() == 0) {
-//                        BombermanGame.winGame = true;
-//                        BombermanGame.playSound(BombermanGame.clipExitOpen);
-//                    }
-//                    return;
-//                }
-//
-//                i.setDeath(true);
-//                BombermanGame.playSound(BombermanGame.clipitemGet);
-//                switch (i.getType()) {
-//                    case (3):
-//                        this.setUpSpeed(true);
-//                        break;
-//                    case (2):
-//                        Bomb.upBombLen();
-//                        break;
-//                    case (1):
-//                        this.upLimitBomb();
-//                        break;
-//                }
-//                return;
-//            }
-//        }
+    public static void resetBombLimit() {
+        bombLimit = 1;
+    }
+
+    public static void resetSpeed() {
+        speed = 1;
     }
 
     public void decLife() {
@@ -326,10 +346,11 @@ public class Bomber extends Entity {
 //            return;
 //        }
 
-        /*Drop the bomb*/
+        //Thả bom
         if (BombermanGame.dropBomb == true && BombermanGame.preDropBomb == false) {
             dropBomb(l);
         }
+
         // SPACE lasts ... ns
         BombermanGame.preDropBomb = BombermanGame.dropBomb;
 
@@ -360,7 +381,7 @@ public class Bomber extends Entity {
             setImg(constImage.get(dir).get(curState));
         }
 
-        /* get item */
+        //Nhặt item
         getBonus();
     }
 }
--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -5,12 +5,8 @@ import javafx.scene.image.Image;
 import uet.oop.bomberman.graphics.Sprite;
 
 public abstract class Entity {
-    //Tọa độ X tính từ góc trái trên trong Canvas
     protected int x;
-
-    //Tọa độ Y tính từ góc trái trên trong Canvas
     protected int y;
-
     protected Image img;
     protected int dir; //Hướng
     protected int curState; //Trạng thái của ảnh
